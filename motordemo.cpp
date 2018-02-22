dbd31a142079727b018f9e69ecc8ce07c45b63ec
Jakub Mandula <jakub@mandula.cz>
2018-02-22 08:46:31 +0000

    add demo routine

--- motordemo.cpp
+++ motordemo.cpp
@@ -4,6 +4,10 @@
 #include "Timer.h"
 #include "mbed.h"
 
+#define LEFT_SCALE 1
+#define RIGHT_SCALE 1
+#define DEMOTIME 1
+
 PwmOut motorLeft(PC_8);
 PwmOut motorRight(PC_9);
 
@@ -14,6 +18,9 @@ DigitalOut motorLeftDir(PB_5);
 DigitalOut motorRightDir(PA_10);
 
 DigitalOut motorEn(PA_5);
+
+InterruptIn startBtn(PC_13);
+
 
 Serial esp(PA_11, PA_12);
 
@@ -32,52 +39,144 @@ enum Direction {
 
 double speed = 0.5;
 Direction direction = stop;
+bool demoRunning = false;
 
-void callback() { esp.printf("Got serial\\n\n"); }
+void startDemo(){
+  demoRunning = true;
+  
+  // Move forward
+  motorEn = 1;
+  motorLeftDir = 0;
+  motorRightDir = 0;
+  motorLeft.write(0.7*LEFT_SCALE);
+  motorRight.write(0.7*RIGHT_SCALE);
+  
+  wait(DEMOTIME);
+  //Stop
+  motorEn = 0;
+  
+  wait(0.2*DEMOTIME);
+  
+  // Move Back
+  motorEn = 1;
+  motorLeftDir = 1;
+  motorRightDir = 1;
+  motorLeft.write(0.7*LEFT_SCALE);
+  motorRight.write(0.7*RIGHT_SCALE);
+  
+  wait(DEMOTIME);
+  motorEn = 0;
+  
+  wait(0.2*DEMOTIME);
+  
+  // Move Rigth
+  motorEn = 1;
+  motorLeftDir = 0;
+  motorRightDir = 0;
+  motorLeft.write(0.7*LEFT_SCALE);
+  motorRight.write(0.3*RIGHT_SCALE);
+  
+  wait(DEMOTIME);
+  motorEn = 0;
+  
+  wait(0.2*DEMOTIME);
+  
+  
+  // Move Right Back
+  motorEn = 1;
+  motorLeftDir = 1;
+  motorRightDir = 1;
+  motorLeft.write(0.7*LEFT_SCALE);
+  motorRight.write(0.3*RIGHT_SCALE);
+  
+  wait(DEMOTIME);
+  motorEn = 0;
+  
+  wait(0.2*DEMOTIME);
+  
+  // Move Left
+  motorEn = 1;
+  motorLeftDir = 0;
+  motorRightDir = 0;
+  motorLeft.write(0.3*LEFT_SCALE);
+  motorRight.write(0.7*RIGHT_SCALE);
+  
+  wait(DEMOTIME);
+  motorEn = 0;
+  
+  wait(0.2*DEMOTIME);
+  
+  
+  // Move Left Back
+  motorEn = 1;
+  motorLeftDir = 1;
+  motorRightDir = 1;
+  motorLeft.write(0.3*LEFT_SCALE);
+  motorRight.write(0.7*RIGHT_SCALE);
+  
+  demoRunning = false;
+}
+
 
 void updateMotors(Direction dir) {
   switch (dir) {
-  case forward:
-    motorEn = 1;
-    motorLeftDir = 0;
-    motorRightDir = 0;
-    motorLeft.write(speed);
-    motorRight.write(speed);
-    break;
-  case backward:
-    motorEn = 1;
-    motorLeftDir = 1;
-    motorRightDir = 1;
-    motorLeft.write(speed);
-    motorRight.write(speed);
-    break;
-  case leftFturn:
-    motorEn = 1;
-    motorLeftDir = 0;
-    motorRightDir = 0;
-    motorLeft.write(speed);
-    motorRight.write(0.5 * speed);
-    break;
-  case rightFturn:
-    motorEn = 1;
-    motorLeftDir = 0;
-    motorRightDir = 0;
-    motorLeft.write(0.5*speed);
-    motorRight.write(speed);
-    break;
-  case leftBturn:
-    break;
-  case rightBturn:
-    break;
-  case stop:
-    motorEn = 0;
-    break;
+    case forward:
+      motorEn = 1;
+      motorLeftDir = 0;
+      motorRightDir = 0;
+      motorLeft.write(speed);
+      motorRight.write(speed);
+      break;
+    case backward:
+      motorEn = 1;
+      motorLeftDir = 1;
+      motorRightDir = 1;
+      motorLeft.write(speed);
+      motorRight.write(speed);
+      break;
+    case leftFturn:
+      motorEn = 1;
+      motorLeftDir = 0;
+      motorRightDir = 0;
+      motorLeft.write(speed);
+      motorRight.write(0.5 * speed);
+      break;
+    case rightFturn:
+      motorEn = 1;
+      motorLeftDir = 0;
+      motorRightDir = 0;
+      motorLeft.write(0.5*speed);
+      motorRight.write(speed);
+      break;
+    case leftBturn:
+      break;
+    case rightBturn:
+      break;
+    case stop:
+      motorEn = 0;
+      break;
   }
 }
 
 int main() {
+  esp.baud(115200);
+  //esp.attach(&callback);
+  
+  startBtn.rise(&startDemo);
+  
+  motorLeftBp = 0;
+  motorRightBp = 0;
+   
+  motorLeftDir = 0;
+  motorRightDir = 0;
+   
+  motorLeft.period(1.0/20000);
+  motorRight.period(1.0/20000);
+   
+  motorEn = 0;
+  
   while (1) {
-    if (esp.readable()) {
+    if (esp.readable() && !demoRunning) {
       switch (esp.getc()) {
       case '\n':
         break;
@@ -103,6 +202,9 @@ int main() {
       case 'e':
         if (speed - 0.1 >= 0)
           speed -= 0.1;
+        break;
+      case 'x':
+        startDemo();
         break;
       }
     }
