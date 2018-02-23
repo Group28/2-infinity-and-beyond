058995e74ea8df6dcd27b40529a0352ee27c47ec
Jakub Mandula <jakub@mandula.cz>
2018-02-23 13:55:51 +0000

    add Sam's starter files

---
+++ System_Functions.h
@@ -0,0 +1,29 @@
+#include "stm32f4xx_ll_bus.h"
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_ll_system.h"
+#include "stm32f4xx_ll_utils.h"
+#include "stm32f4xx_ll_pwr.h"
+#include "stm32f4xx_ll_exti.h"
+#include "stm32f4xx_ll_gpio.h"
+#include "stm32f4xx_ll_adc.h"
+#include "stm32f4xx_ll_cortex.h"
+#include "stm32f4xx_ll_crc.h"
+#include "stm32f4xx_ll_dac.h"
+#include "stm32f4xx_ll_dma.h"
+#include "stm32f4xx_ll_dma2d.h"
+#include "stm32f4xx_ll_i2c.h"
+#include "stm32f4xx_ll_iwdg.h"
+#include "stm32f4xx_ll_rtc.h"
+#include "stm32f4xx_ll_spi.h"
+#include "stm32f4xx_ll_tim.h"
+#include "stm32f4xx_ll_usart.h"
+#include "stm32f4xx_ll_wwdg.h"
+#include "stm32f4xx_ll_rng.h"
+#include "stm32f4xx_ll_lptim.h"
+
+void SystemClock_Config(void);
+void Configure_USB_UART(void);
+void Configure_User_LED(void);
+void Configure_User_Button(void);
+
+
