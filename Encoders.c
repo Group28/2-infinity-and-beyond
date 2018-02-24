3ee2c8a84a02ea6e62ec49e1fdb8284d856693bd
Jakub Mandula <jakub@mandula.cz>
2018-02-24 23:29:59 +0000

    implement Encoder sampling

--- Encoders.c
+++ Encoders.c
@@ -1,15 +1,40 @@
 #include "Encoders.h"
 #include "math.h"
+#include "stdlib.h"
+#include "configuration.h"
 
-
-double Encoder_getSpeed(QEncoder * encoder){
-	return 0;
+Encoder * Encoder_init(TIM_TypeDef * timer, double samplePeriod){
+	Encoder * encoder = (Encoder*)malloc(sizeof(Encoder));
+	encoder->speed = 0;
+	encoder->samplePeriod = samplePeriod;
+	encoder->lastCount = 0;
+	encoder->revolutions = 0;
+	encoder->timer = timer;
+	
+	LL_TIM_SetCounter(encoder->timer, 0);
+	return encoder;
 }
 
-int Get_Encoder_Right(void){
-	return LL_TIM_GetCounter(TIM2);
+double Encoder_getSpeed(Encoder * encoder){
+	return encoder->speed;
 }
 
-int Get_Encoder_Left(void){
-	return LL_TIM_GetCounter(TIM5);
+double Encoder_getRevolutions(Encoder * encoder){
+	return encoder->revolutions;
 }
+
+void Encoder_Reset(Encoder * encoder){
+	encoder->speed = 0;
+	encoder->revolutions=0;
+	LL_TIM_SetCounter(encoder->timer, 0);
+}
+
+
+void Encoder_Update(Encoder *encoder){
+	int32_t count = (int32_t) LL_TIM_GetCounter(encoder->timer);
+	
+	encoder->speed = (difference)/(samplePeriod*ENCODER_TICKS_PER_REV);
+	encoder->revolutions = 
+}
+
+void 
