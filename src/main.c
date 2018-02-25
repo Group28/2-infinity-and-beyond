1c1cf0d74e9390517655895afbe232c085140cf7
Jakub Mandula <jakub@mandula.cz>
2018-02-25 08:14:26 +0000

    rename PID values for motors

--- src/main.c
+++ src/main.c
@@ -28,18 +28,18 @@ int main(void)
 	IO_init();
 	
 	// Initialize PID controllers for the Motors
-	PID_Values motorLeft = {10, 1, 1};
-	PID_Values motorRight = {10, 1, 1};
+	PID_Values motorLeftPID = {10, 1, 1};
+	PID_Values motorRightPID = {10, 1, 1};
 	
+	// Initialize peripherals
 	Motors * motors;
 	Encoder * encoderLeft, encoderRight;
 	LCD * lcd;
 	USART * usb, esp;
-  
 	
 	encoderLeft = Encoder_init(TIM5, 1/MOTOR_SAMPLE_FREQ);
 	encoderRight = Encoder_init(TIM2, 1/MOTOR_SAMPLE_FREQ);
-	motors = Motors_init(motorLeft, motorRight, encoderLeft, encoderRight);
+	motors = Motors_init(motorLeft, motorRight, encoderLeftPID, encoderRightPID);
 	lcd = LCD_init();
 	usb = USART_USB_init();
 	esp = USART_ESP_init();
