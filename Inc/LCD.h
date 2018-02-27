85b7450edee7dbcdbdce911310ec907ad87f7b75
Jakub Mandula <jakub@mandula.cz>
2018-02-27 13:21:50 +0000

    update LCD driver, optimize buffer copy

--- Inc/LCD.h
+++ Inc/LCD.h
@@ -58,5 +58,74 @@ void     __LCD_writeCommand(unsigned char command);
 
 void     __LCD_copyDataBuffer(LCD* lcd);
 
+// Displa ON/OFF
+#define __LCD_CMD_DISPLAY_OFF         0xAE
+#define __LCD_CMD_DISPLAY_ON          0xAF
+
+// Display start line set
+#define __LCD_CMD_DISPLAY_SL          0x40 // and in 6-bit address
+
+// Page address set
+#define __LCD_CMD_PAGE_ADDR           0xB0
+
+// Column address set lower and upper bit
+#define __LCD_CMD_LOWER_BIT           0x00
+#define __LCD_CMD_UPPER_BIT           0x10
+
+
+// ADC select/
+#define __LCD_CMD_ADC_NORMAL          0xA0
+#define __LCD_CMD_ADC_REVERSE         0xA1
+
+// LCD bias set
+#define __LCD_CMD_LCD_BIAS_1_9        0xA2
+#define __LCD_CMD_LCD_BIAS_1_7        0xA3
+
+// Display all points ON/OFF
+#define __LCD_CMD_DISP_POINTS_NORMAL  0xA4
+#define __LCD_CMD_DISP_POINTS_ALL     0xA5
+
+// Display normal reverse
+#define __LCD_CMD_DISP_NORMAL         0xA6
+#define __LCD_CMD_DISP_REVERSE        0xA7
+
+// Read-modify-write
+#define __LCD_CMD_CAI_READ            0xE0
+#define __LCD_CMD_CAI_WRITE           0xE1
+
+//End
+#define __LCD_CMD_END                 0xEE
+
+// Reset
+#define __LCD_CMD_RESET               0xE2
+
+// Common output mode select
+#define __LCD_CMD_COMMON_OUT_NORMAL   0xC0
+#define __LCD_CMD_COMMON_OUT_REVERSE  0xC8
+
+// Power control set
+#define __LCD_CMD_PWR_MODE            0x28
+
+// Vo voltage regulator internal resistor ratio set
+#define __LCD_CMD_RESISTOR_RATIO      0x20
+
+// Electronic volume mode/register set
+#define __LCD_CMD_CONTRAST            0x81
+
+// Sleep mode set
+#define __LCD_CMD_SLEEP_ON            0xAC
+#define __LCD_CMD_SLEEP_OFF           0xAD
+
+// Booster ratio set
+#define __LCD_CMD_BOOSER_RATIO1       0xF8
+#define __LCD_CMD_BOOSER_RATIO2_2X    0x00
+#define __LCD_CMD_BOOSER_RATIO2_5X    0x01
+#define __LCD_CMD_BOOSER_RATIO2_6X    0x03
+// NOP
+
+#define __LCD_CMD_NOP                 0xE3
+
+
+
 
 #endif
