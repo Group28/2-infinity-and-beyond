af68998907eab28580d5ed52918755f03a3ccc73
Jakub Mandula <jakub.aludnam@gmail.com>
2018-02-28 19:59:03 +0100

    Make private functions static and move them to c files

--- Inc/IO.h
+++ Inc/IO.h
@@ -20,17 +20,5 @@ typedef struct{
 // Initialize all IO devices
 void IO_init(void);
 
-// Initialize an output
-void IO_Output_init(GPIO_TypeDef * port, uint32_t pin);
-//void IO_Analog_init();
 
-void IO_initMotors(void);
-void IO_initEncoders(void);
-void IO_initAnalogueSensors(void);
-
-void IO_initLCD(void);
-void IO_initUSART_USB(void);
-void IO_initUSART_ESP(void);
-
-void IO_initJoyButton(void);
 #endif
