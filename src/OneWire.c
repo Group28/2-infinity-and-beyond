d7b8c994ea1221e7c7081422f963726314d191f9
Jakub Mandula <jakub@mandula.cz>
2018-03-12 17:16:03 +0000

    add ds2781 support

--- src/OneWire.c
+++ src/OneWire.c
@@ -1,5 +1,37 @@
 #include "OneWire.h"
 #include "stdlib.h"
+#include "utils.h"
+
+
+#define OW_OUTPUT(ow) LL_GPIO_SetPinMode(ow->port, ow->pin, LL_GPIO_MODE_OUTPUT);
+#define OW_INPUT(ow)  LL_GPIO_SetPinMode(ow->port, ow->pin, LL_GPIO_MODE_INPUT);
+
+
+#define OW_Delay_A()	delay_us(5);
+#define OW_Delay_B()  delay_us(64);
+#define OW_Delay_C()	delay_us(60);
+#define OW_Delay_D()	delay_us(10);
+
+#define OW_Delay_E()	delay_us(10);
+#define OW_Delay_F()	delay_us(55);
+
+//Delays Used for OWReset (Confirmed Working)
+#define OW_Delay_H()	delay_us(480);
+#define OW_Delay_I()	delay_us(68);
+#define OW_Delay_J()	delay_us(405);
+
+
+#define OW_Delay_TimeSlot() delay_us(6)
+#define OW_Delay_Recovery() delay_us(1)
+#define OW_Delay_Write0LT() delay_us(6)
+#define OW_Delay_Write1LT() delay_us(1)
+#define OW_Delay_ReadData() delay_us(2)
+#define OW_Delay_ResetTHi() delay_us(48)
+#define OW_Delay_ResetTLo() delay_us(48)
+#define OW_Delay_PresenceH() delay_us(2)
+#define OW_Delay_PresenceL() delay_us(8)
+
+
 
 
 
@@ -8,7 +40,7 @@ OW OW_init(GPIO_TypeDef * port, uint32_t pin){
   ow->port = port;
   ow->pin = pin;
   
-  __OW_INPUT();
+  OW_INPUT(ow);
   LL_GPIO_SetPinSpeed(port, pin, LL_GPIO_SPEED_FREQ_VERY_HIGH);
   LL_GPIO_SetPinPull(port, pin, LL_GPIO_PULL_DOWN);
   LL_GPIO_ResetOutputPin(port, pin);
@@ -22,32 +54,32 @@ bool OW_reset(OW ow){
   bool result = false;
   
   LL_GPIO_ResetOutputPin(ow->port, ow->pin);
-  __OW_OUTPUT();
+  OW_OUTPUT(ow);
   
-  __OW_Delay_H();
+  OW_Delay_H();
   
-  __OW_INPUT();
+  OW_INPUT(ow);
   
-  __OW_Delay_I();
+  OW_Delay_I();
   
   if(LL_GPIO_IsInputPinSet(ow->port, ow->pin)) result = true;
   
-  __OW_Delay_J();
+  OW_Delay_J();
   return result;
 }
 
 void OW_writeBit(OW ow, BYTE b){
   LL_GPIO_ResetOutputPin(ow->port, ow->pin);
-  __OW_OUTPUT();
+  OW_OUTPUT(ow);
   if(b){
-    __OW_Delay_A();
-    __OW_INPUT();
-    __OW_Delay_B();
+    OW_Delay_A();
+    OW_INPUT(ow);
+    OW_Delay_B();
     
   } else {
-    __OW_Delay_C();
-    __OW_INPUT();
-    __OW_Delay_D();
+    OW_Delay_C();
+    OW_INPUT(ow);
+    OW_Delay_D();
   }
   
 }
@@ -56,15 +88,15 @@ BYTE OW_readBit(OW ow){
   BYTE result;
   
   LL_GPIO_ResetOutputPin(ow->port, ow->pin);
-  __OW_OUTPUT();
+  OW_OUTPUT(ow);
   
-  __OW_Delay_A();
+  OW_Delay_A();
   
-  __OW_INPUT();
-  __OW_Delay_E();
+  OW_INPUT(ow);
+  OW_Delay_E();
   result = (BYTE) LL_GPIO_IsInputPinSet(ow->port, ow->pin);
   
-  __OW_Delay_F();
+  OW_Delay_F();
   
   return result & 0x01;
 }
